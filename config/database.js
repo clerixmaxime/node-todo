bdcac1f017f336bc4df2ab08c1f4c12021a45853
Maxime Clerix <clerixmaxime@users.noreply.github.com>
2017-03-27 17:02:02 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://mongo:mongo@mongo-todo:27017/todo',
+    localUrl: 'mongodb://mongo-todo/todo'
 };
