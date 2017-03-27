274530f708736aaf99cb3d9b1884a7adcadc6b9e
Maxime Clerix <solidsnake444@hotmail.fr>
2017-03-27 17:33:14 +0200

    /

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://mongo:mongo@mongo-todo:27017/todo',
-    localUrl: 'mongodb://mongo:mongo@mongo-todo:27017/todo'
+    remoteUrl : 'mongodb://mongo:mongo@mongo-todo:27017/mongo',
+    localUrl: 'mongodb://mongo:mongo@mongo-todo:27017/mongo'
 };
