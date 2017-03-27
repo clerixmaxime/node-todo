1d9a0e0beea3c5129c9d00e13712ed79c29a7cb4
Maxime Clerix <clerixmaxime@users.noreply.github.com>
2017-03-27 17:03:03 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://mongo:mongo@mongo-todo:27017/todo',
-    localUrl: 'mongodb://mongo-todo/todo'
+    localUrl: 'mongodb://mongo:mongo@mongo-todo:27017/todo'
 };
