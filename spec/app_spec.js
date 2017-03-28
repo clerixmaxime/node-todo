aa67d48336fd4f376cda6b3e7c23f7eeb71e60dd
Maxime Clerix <solidsnake444@hotmail.fr>
2017-03-28 11:22:27 +0200

    Add unit tests

---
+++ spec/app_spec.js
@@ -0,0 +1,12 @@
+describe("App", function() {
+
+  it("is creating Todo", function() {
+    expect(true).toBe(true);
+  });
+  it("is deleting Todo", function() {
+    expect(true).toBe(true);
+  });
+  it("is getting Todo", function() {
+    expect(true).toBe(true);
+  });
+});
